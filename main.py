9a099324a327442081be21d88a67dd972420f00f
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-18 15:30:01 +0100

    Fixed code for histogram

--- main.py
+++ main.py
@@ -6,23 +6,23 @@ import scipy.stats
 
 def binomial(n,p) :
   # Your code to generate the binomial random variables goes here.
-  
+ 
+nsamples = 500 
 histo = np.zeros(6)
-# Insert code to compute a histogram by generating 500 binomial random variables with
+# Insert code to compute a histogram by generating nsamples binomial random variables with
 # n=5 and p=0.5 here.  
 
 # Don't forget to normalise your histogram.
 
 
 # Include the code to compute the error bars at the 90% confidence limit here.  The list
-# called lower should contain the difference between the mean and the 5th percentile of the
-# distributions.  The list called upper should contain the differenc between the 95th percentile 
-# of the distribution and the mean.
-lower, upper = np.zeros(6), np.zeros(6)
+# called error should contain the difference between the 95th percentile for the distribution of the 
+# mean and the mean 
+error = np.zeros(6) 
 
 
 # This will plot the histogram and the error bars
 plt.bar( [0,1,2,3,4,5], histo, width=0.1 )
 # This plots the small bar around each of the values.
-plt.errorbar( [0,1,2,3,4,5], histo, yerr=[lower,upper], fmt='ko' )
+plt.errorbar( [0,1,2,3,4,5], histo, yerr=error, fmt='ko' )
 plt.savefig("histo_with_errors.png")
---
+++ testing/test_main.py
@@ -0,0 +1,55 @@
+try:
+    from AutoFeedback.plotchecks import check_plot
+except:
+    import subprocess
+    import sys
+
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "AutoFeedback"])
+    from AutoFeedback.plotchecks import check_plot
+
+from AutoFeedback.plotclass import line
+from AutoFeedback.funcchecks import check_func
+from AutoFeedback.randomclass import randomvar
+import unittest
+from main import *
+
+class errclass : 
+   def get_error(i) : 
+       from scipy.stats import norm
+       return ( error[i] / norm.ppf(0.95) )**2
+
+class UnitTests(unittest.TestCase) :
+    def test_binom(self) :
+        inputs, variables = [], []
+        for n in range(3,6) :
+            for i in range(1,9) :
+                p = i*0.1
+                inputs.append((n,i*0.1,))
+                myvar = randomvar( n*p, variance=n*p*(1-p), vmin=0, vmax=n, isinteger=True )
+                variables.append( myvar )
+        assert( check_func('binomial',inputs, variables, calls=['bernoulli'] ) ) 
+
+    def test_error(self) :
+        inputs, variables = [], [] 
+        for i in range(6) :
+            inputs.append((i,))
+            pval = binom.pmf(i, 5, 0.5 )
+            myvar = randomvar( pval, dist="chi2", variance=pval*(1-pval)/nsamples, isinteger=False)
+            variables.append( myvar )
+        assert( check_func("get_error", inputs, variables, modname=errclass ) )  
+
+    def test_plot(self) :
+        x, e, var, bmin, bmax, isi  = [], [], [], [], [], []
+        for i in range(6) :
+            x.append(i)
+            pval = binom.pmf(i, 5, 0.5 )
+            e.append(pval)
+            var.append(pval*(1-pval)/nsamples)
+            bmin.append(0)
+            bmax.append(1)
+            isi.append(False)
+        
+        val = randomvar( e, variance=var, vmin=bmin, vmax=bmax, isinteger=isi )
+        line1=line( x, val )
+        axislabels=["Outcome", "Fraction of occurances"] 
+        assert(check_plot([line1],exppatch=line1,explabels=axislabels,explegend=False,output=True))
